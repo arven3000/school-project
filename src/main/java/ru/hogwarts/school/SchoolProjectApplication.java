d5756619ef78c81c7e8099c2d5cceccd0be4183a
Natali-Kremneva <van2gog6@gmail.com>
2022-06-03 21:42:39 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/SchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolProjectApplication.class, args);
+    }
+
+}
